0a9c963babb785498064fff15bf65fdfcbe1e1bf
AshuNaik08 <veena08naikar@gmail.com>
2025-05-02 12:19:34 +0530

    Test

---
+++ AccessSpecifier/AccessSpecifier/src/test/java/p3/Test1.java
@@ -0,0 +1,15 @@
+package p3;
+
+public class Test1 {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=0;i<=20;i++)
+        {
+            if(i%2==0)
+            {
+                sum=sum+1;
+            }
+        }
+        System.out.println(sum);
+    }
+}
