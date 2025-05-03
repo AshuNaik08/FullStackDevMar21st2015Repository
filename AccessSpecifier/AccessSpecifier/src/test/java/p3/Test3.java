75788132ce5abfc371545bffb2ab288d0d79d9f9
AshuNaik08 <veena08naikar@gmail.com>
2025-05-03 09:24:30 +0530

    Prime

---
+++ AccessSpecifier/AccessSpecifier/src/test/java/p3/Test3.java
@@ -0,0 +1,15 @@
+package p3;
+
+public class Test3 {
+    public static void main(String[] args) {
+        int num=4;
+
+            if(num%2!=0)
+            {
+                System.out.println("It is Prime Number");
+            }
+            else {
+                System.out.println("It is not a Prime Number");
+            }
+    }
+}
